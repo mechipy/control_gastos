aec5c0729d2cbac680a7905fd78dc3ca45e1a7fd
mechipy <mercepereyra51@gmail.com>
2020-11-01 03:58:37 -0300

    se agrega función para seleccionar una opcion del menú

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print(" hola mundo ")
+def devolver_opcion():
+    print("""
+    ***************************
+    *    CONTROL DE GASTOS    *
+    *************************** """)
+
+    opcion = input("""
+    1. Agregar Gasto
+    2. Agregar Saldo
+    3. Consultar Gastos
+    4. Consultar Saldo
+    5. Salir
+    Elija una opción [1-5]: """)
+    return opcion
