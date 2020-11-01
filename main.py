2f5dc449ac83d07bec19d660eb3bcf9a48045640
mechipy <mercepereyra51@gmail.com>
2020-11-01 04:28:10 -0300

    se agregó un dict con clave como número y el valor es el mes como archivo

--- main.py
+++ main.py
@@ -1,3 +1,18 @@
+MESES = {
+    "1": "data/enero.txt",
+    "2": "data/febrero.txt",
+    "3": "data/marzo.txt",
+    "4": "data/abril.txt",
+    "5": "data/mayo.txt",
+    "6": "data/junio.txt",
+    "7": "data/julio.txt",
+    "8": "data/agosto.txt",
+    "9": "data/septiembre.txt",
+    "10": "data/octubre.txt",
+    "11": "data/noviembre.txt",
+    "12": "data/diciembre.txt"}
+
+
 def devolver_opcion():
     print("""
     ***************************
